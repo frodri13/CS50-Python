477e5e80dc63bc80acaf7ca0af6bd1d5515f3ac5
Fabio Rodriguez <alessf13@gmail.com>
2022-04-15 20:06:40 +0100

    cooler version

--- cat.py
+++ cat.py
@@ -3,6 +3,22 @@
 #     print("meow")
 #     i += 1
 
-# for i in [0, 1, 2]:
-for _ in range(3):
-    print("meow")
+# # for i in [0, 1, 2]:
+# for _ in range(3):
+#     print("meow")
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What is n? "))
+        if n > 0:
+            return n
+
+def meow(n):
+    for _ in range(n):
+        print("meow!")
+
+main()
